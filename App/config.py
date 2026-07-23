5f6e9b3faaf22e0fbfe14ed61f8e6054e9ae9b84
Brotbeutel <jannik_schlueter@hotmail.de>
2026-07-23 15:32:18 +0200

    Update docs, add .env.example, and environment-driven mail config

--- App/config.py
+++ App/config.py
@@ -31,14 +31,14 @@ class Config:
     REMEMBER_COOKIE_DURATION = timedelta(days=7)
 
     # --- E-Mail (Flask-Mail) ---
-    MAIL_SERVER = "127.0.0.1"
-    MAIL_PORT = 1025
-    MAIL_USE_TLS = False
-    MAIL_USERNAME = ""
-    MAIL_PASSWORD = ""
-    MAIL_DEFAULT_SENDER = "kundenverwaltung@sportless-gmbh.de"
+    MAIL_SERVER = os.environ.get("MAIL_SERVER", "127.0.0.1")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", "1025"))
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "False").lower() in ("1", "true", "yes", "on")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER", "kundenverwaltung@sportless-gmbh.de")
 
-    MAIL_SUPPRESS_SEND = False
+    MAIL_SUPPRESS_SEND = os.environ.get("MAIL_SUPPRESS_SEND", "False").lower() in ("1", "true", "yes", "on")
 
 
 class EntwicklungConfig(Config):
